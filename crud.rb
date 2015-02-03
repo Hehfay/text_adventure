58606328aef3f45ec67a1e5827b461fe0ce3fa7f
Hehfay <jeffrey.hartman4691@gmail.com>
2015-02-03 11:21:11 -0500

    Removed tabs.

--- crud.rb
+++ crud.rb
@@ -2,70 +2,70 @@
 # TODO This class performs CRUD operations on a file.
 # Eventually, this will need to be updated to store
 # game data for text adventure, but this is a good
-# start.
+# start. Probably need to look at YAML.
 class Crud
-	
-	public
 
-	def Crud.initialize(file_name)
-		@file_name = file_name
-		@record = {}
-		update_hash if File.exists?(@file_name)  
-	end
+  public
 
-	def Crud.create(name, rating)
-		if @record.has_key?(name.to_sym)
-			"#{name} record already exists."
-		else
-			@record[name.to_sym] = rating.to_i
-			update_file('a+')
-			"#{name} #{rating} added."
-		end
-	end
+  def Crud.initialize(file_name)
+    @file_name = file_name
+    @record = {}
+    update_hash if File.exists?(@file_name)  
+  end
 
-	def Crud.read
-		counter = 1
-		@record.each do |key, value| 
-			printf("%.2i %s %i\n", counter, key, value) 
-			counter += 1
-		end
-	end
+  def Crud.create(name, rating)
+    if @record.has_key?(name.to_sym)
+      "#{name} record already exists."
+    else
+      @record[name.to_sym] = rating.to_i
+      update_file('a+')
+      "#{name} #{rating} added."
+    end
+  end
 
-	def Crud.update(key, value)
-		if @record.has_key?(key.to_sym)
-			@record[key.to_sym] = value.to_i
-			update_file
-			'Update successful.'
-		else
-			"#{key} not found."
-		end
-	end
+  def Crud.read
+    counter = 1
+    @record.each do |key, value| 
+      printf("%.2i %s %i\n", counter, key, value) 
+      counter += 1
+    end
+  end
 
-	def Crud.delete(key)
-		if @record.has_key?(key.to_sym)
-			@record.delete(key.to_sym)
-			update_file
-			'Delete successful.'
-		else
-			"#{key} not found."
-		end
-	end
+  def Crud.update(key, value)
+    if @record.has_key?(key.to_sym)
+      @record[key.to_sym] = value.to_i
+      update_file
+      'Update successful.'
+    else
+      "#{key} not found."
+    end
+  end
 
-	private
+  def Crud.delete(key)
+    if @record.has_key?(key.to_sym)
+      @record.delete(key.to_sym)
+      update_file
+      'Delete successful.'
+    else
+      "#{key} not found."
+    end
+  end
 
-	def update_file(opening_mode = 'w')
-		f = File.open(@file_name, opening_mode)
-		@record.each { |key, value| f.puts "#{key} #{value}" }
-		f.close
-	end
+  private
 
-	def update_hash
-		f = File.open(@file_name, 'r')
-			f.each_line do |line|
-				word = line.split(' ')
-				@record[word[0].to_sym] = word[1].to_i
-			end
-		f.close
-	end
+  def update_file(opening_mode = 'w')
+    f = File.open(@file_name, opening_mode)
+    @record.each { |key, value| f.puts "#{key} #{value}" }
+    f.close
+  end
+
+  def update_hash
+    f = File.open(@file_name, 'r')
+    f.each_line do |line|
+      word = line.split(' ')
+      @record[word[0].to_sym] = word[1].to_i
+    end
+    f.close
+  end
 
 end
--- player.rb
+++ player.rb
@@ -1,31 +1,31 @@
 class Player < Character
 
-	public
+  public
 
-	def initialize(name)
-		# Initialize will need to set starting values
-		# if starting a new game. Else it will need
-		# to read values from a file using class Crud
-		# (loading a saved game).
-	end
+  def initialize(name)
+    # Initialize will need to set starting values
+    # if starting a new game. Else it will need
+    # to read values from a file using class Crud
+    # (loading a saved game).
+  end
 
-	def battle_victory(experience)
-	end
+  def battle_victory(experience)
+  end
 
-	def equip_weapon(new_weapon)
-	end
+  def equip_weapon(new_weapon)
+  end
 
-	def equip_armor(new_armor)
-	end
+  def equip_armor(new_armor)
+  end
 
 
 
-	private
+  private
 
-	def level_up
-		# Health, strength, endurance, level,
-		# and experience attributes will 
-		# be modified here.
-	end
+  def level_up
+    # Health, strength, endurance, level,
+    # and experience attributes will 
+    # be modified here.
+  end
 
 end
