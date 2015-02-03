f41bcff0b8d90f8ed61220892613102dda5205f1
Hehfay <jeffrey.hartman4691@gmail.com>
2015-02-03 11:08:54 -0500

    Finally got the cursor to move correctly in game_window.rb

--- game_window.rb
+++ game_window.rb
@@ -1,51 +1,53 @@
+#!/usr/bin/env ruby
+
 class Game_Window
 
   require 'curses'
   include Curses
 
-  HEIGHT_OF_BOTTOM_BOX = 0.33 
+  HEIGHT_OF_CONTROL = 0.33 # 1/3 of the screen
 
-  def get_max_xy 
-    Curses.init_screen
+  def Game_Window.get_max_xy 
     window = Curses::Window.new(0, 0, 0, 0)
-    x = window.maxx
-    y = window.maxy
-    array = [x, y]
+    @@x = window.maxx
+    @@y = window.maxy
+    @@control_size = @@y * HEIGHT_OF_CONTROL
     window.close
-    return array
   end
 
-  def draw_display(array)
-    @x = array[0]
-    @y = array[1]
-    Curses.init_screen
-    Curses.noecho
-    Curses.curs_set(1)
-
-    @control = Curses::Window.new( (@y * HEIGHT_OF_BOTTOM_BOX), 0, (@y - (@y * HEIGHT_OF_BOTTOM_BOX) + 1), 0 )
-    @control.box('|', '-')
-    @control.refresh
+  def Game_Window.draw_display
+    display ||= Curses::Window.new(@@y - @@control_size, 0, 0, 0)
+    display.box('|', '-')
+    display.refresh
+    return display
   end
 
-  def populate_screen
-    @control.setpos(2, (@x * 0.20))
-    @control.addstr('Option 1')
-    @control.setpos(2, (@x * 0.40))
-    @control.addstr('Option 2')
-    @control.setpos(2, (@x * 0.60))
-    @control.addstr('Option 3')
+  def Game_Window.draw_control
+    control ||= Curses::Window.new(@@control_size, 0, (@@y - @@control_size), 0)
+    control.box('|', '-')
+    control.refresh
+    return control
   end
 
-  def on_close
-    @control.close
+  def Game_Window.populate_screen(window)
+    window.setpos(2, (@@x * 0.20))
+    window.addstr('Option 1' || A_PROTECTED)
+    window.setpos(2, (@@x * 0.60))
+    window.addstr('Option 2' || A_PROTECTED)
+    window.refresh
   end
 
-  def move_cursor
-    Curses.cbreak
-    Curses.curs_set(0)
-    @control.keypad(true)
-    @control.setpos(2, (@x * 0.20) - 1 )
-    input = @control.getch
+  def Game_Window.close(window)
+    window.clear
+    window.refresh
+    window.close
+  end
+
+  def Game_Window.move_cursor(window)
+    window.setpos(2, (@@x * 0.20) - 1)
+    window.insch('>')
+    window.refresh
+    input = window.getch
     until input == Curses::KEY_UP  
       case 
       when input == Curses::KEY_BACKSPACE
@@ -53,26 +55,50 @@ class Game_Window
       when input == Curses::KEY_DOWN
         Curses.flash
       when input == Curses::KEY_LEFT
-        @control.delch
-        @control.setpos(2, (@x * 0.20) - 1)
-        @control.insch('>')
-        @control.refresh
+        window.delch
+        window.insch(' ')
+        window.setpos(2, (@@x * 0.20) - 1)
+        window.delch
+        window.insch('>')
+        window.refresh
       when input == Curses::KEY_RIGHT
-        @control.delch
-        @control.setpos(2, (@x * 0.60) - 1)
-        @control.insch('>')
-        @control.refresh
+        window.delch
+        window.insch(' ')
+        window.setpos(2, (@@x * 0.60) - 1)
+        window.delch
+        window.insch('>')
+        window.refresh
       else
         Curses.flash
       end
-      input = @control.getch
+      input = window.getch
     end
+  end
+
+  def set_term_mode
+    Curses.cbreak
+    Curses.curs_set(0)
+    @control.keypad(true)
+    @control.setpos(2, (@x * 0.20) - 1 )
   end
 
 end
 
-main_window = Game_Window.new
-main_window.draw_display(main_window.get_max_xy)
-main_window.populate_screen
-main_window.move_cursor
-main_window.on_close
+Curses.init_screen
+Curses.noecho
+Curses.curs_set(2)
+Game_Window.get_max_xy
+
+control = Game_Window.draw_control
+display = Game_Window.draw_display
+
+control.keypad(true)
+display.keypad(false)
+
+Game_Window.populate_screen(control)
+Game_Window.move_cursor(control)
+
+Game_Window.close(display)
+Game_Window.close(control)
+Curses.close_screen
+
